30fecf7d250d7fd1bbd8b9567e97705a3ad9bfbb
Queeniebee <joasqueeniebee@gmail.com>
2014-04-05 16:48:07 -0400

    wrote the cronjob

--- configure.sh
+++ configure.sh
@@ -13,4 +13,11 @@ sudo python scripts/time.py
 # run a cron job here
 1 2 3 4 5 USERNAME /path/to/command arg1 arg2
 
+OR
 
+1 2 3 4 5 USERNAME /path/to/script.sh
+
+
+[minute, hours, day, month, day of week]
+
+0 0-23/2 * * * /path/to/script.sh >/dev/null 2>&1
